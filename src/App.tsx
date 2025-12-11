f9de53509fa019fc607f4aebcf19481af034174a
IIMaksymilianDymitrII <maksymilian.dymitr@chasacademy.se>
2025-12-11 14:05:30 +0100

    Created Forgot Password page and added routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
+import { Routes, Route } from "react-router-dom";
 import LogIn from "./Pages/Login";
 import SignIn from "./Pages/Signin";
+import ForgotPassword from "./Pages/ForgotPassword";
 
 function App() {
   return (
-    <div>
-    <SignIn/>
-    </div>
+    <Routes>
+      <Route path="/" element={<LogIn/>} />
+      <Route path="/signin" element={<SignIn/>}/>
+      <Route path="/forgotpassword" element={<ForgotPassword/>}/>
+    </Routes>
   );
 }
 
--- src/Pages/ForgotPassword.tsx
+++ src/Pages/ForgotPassword.tsx
@@ -1,13 +1,14 @@
 import Wheel from "../assets/racing.png";
-import { Link, NavLink } from "react-router-dom";
+import { Link } from "react-router-dom";
 
-const LogIn = () => {
+const ForgotPassword = () => {
   return (
+
     <div className="flex justify-center items-center w-screen h-screen bg-slate-950">
       <div className="bg-slate-900 rounded-2xl p-3 text-white font-semibold ">
         <div className="text-center p-8 text-2xl flex flex-col items-center justify-center gap-3">
           <img src={Wheel} alt="Logo" className="size-20 invert " />
-          <h1>Log in to your account</h1>
+          <h1>Setup New Password</h1>
         </div>
         <div className="flex flex-col gap-3 px-2">
           <div className="flex flex-col gap-3">
@@ -21,9 +22,6 @@ const LogIn = () => {
             <div className="flex flex-col gap-1 ">
               <div className="flex justify-between">
                 <h2>Password</h2>
-                <p className="text-blue-500 hover:text-blue-400">
-                  <Link to="/forgotpassword">Forgot Password?</Link>
-                </p>
               </div>
               <input
                 type="text"
@@ -31,29 +29,15 @@ const LogIn = () => {
               />
             </div>
           </div>
-          <button className="w-full bg-indigo-700  rounded-lg cursor-pointer p-2 hover:bg-indigo-600">
-            Sign in
+          <button className="w-full my-2 bg-indigo-700  rounded-lg cursor-pointer p-2 hover:bg-indigo-600">
+            <Link to="/">
+          Create New Password
+            </Link>
           </button>
-          <div className="flex gap-3 ">
-            <button className="w-1/2 bg-slate-800 rounded-lg p-2  hover:bg-slate-700">
-              <img src="" alt="" />
-              Google
-            </button>
-            <button className="w-1/2 bg-slate-800 rounded-lg p-2 hover:bg-slate-700">
-              <img src="" alt="" />
-              Apple
-            </button>
-          </div>
-        </div>
-        <div className="p-3 text-center flex justify-around">
-          <p>Not a member?</p>
-          <p className="text-blue-500 hover:text-blue-400">
-            <Link to="/signin" >Become our Member!</Link>
-          </p>
         </div>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default LogIn;
+export default ForgotPassword
