1ffa650e5f9fbbde00cc83afb98c42456e812210
Suhagan Mostahid <55191411+suhagan@users.noreply.github.com>
2025-12-18 01:04:24 +0100

    Sign up operation has taken down due to future aspects. Coming back soon with new and modern systems.

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,6 @@ import HomePage from "./pages/HomePage";
 import SchedulePage from "./pages/SchedulePage";
 import { useTheme } from "./context/ThemeContext";
 
-import Step1Personal from "./pages/Registrationpages/Step1Personal";
-import Step2Address from "./pages/Registrationpages/Step2Address";
-import Step3Contact from "./pages/Registrationpages/Step3Contact";
-import Step4Summary from "./pages/Registrationpages/Step4Summary";
-import { RegistrationFormProvider } from "./context/RegistrationFormContext";
-
-
 const App: React.FC = () => {
   const { themeColors } = useTheme();
   return (
@@ -20,21 +13,6 @@ const App: React.FC = () => {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/schedules" element={<SchedulePage />} />
-
-
-        <Route
-          path="/register/*"
-          element={
-            <RegistrationFormProvider>
-              <Routes>
-                <Route path="step-1" element={<Step1Personal />} />
-                <Route path="step-2" element={<Step2Address />} />
-                <Route path="step-3" element={<Step3Contact />} />
-                <Route path="step-4" element={<Step4Summary />} />
-              </Routes>
-            </RegistrationFormProvider>
-          }
-        />
         
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
